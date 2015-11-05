78f1fd761a971c987b6b10f6250936d5be8404c9
student1 <student1@jh-mbair-158-08.d214.org>
2015-11-05 11:07:08 -0600

    Initial Commit

---
+++ UploadTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UploadTest
+//
+//  Created by student1 on 11/5/15.
+//  Copyright © 2015 John Hersey High school. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
