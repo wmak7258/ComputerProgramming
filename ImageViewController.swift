8603ddca324903069e9356889b66f7c0e4493280
student1 <student1@jh-mbair-158-08.d214.org>
2015-11-06 09:48:49 -0600

    create new ViewController and segues

---
+++ ImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageViewController.swift
+//  UploadTest
+//
+//  Created by student1 on 11/6/15.
+//  Copyright © 2015 John Hersey High school. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
