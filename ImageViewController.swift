2816329040074e5fc7dcecdd54026bb3a6a126dd
student1 <student1@jh-mbair-158-08.d214.org>
2015-11-09 09:52:27 -0600

    make color display with button

--- ImageViewController.swift
+++ ImageViewController.swift
@@ -9,27 +9,23 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
+    
+    var isBlueStar: Bool?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if isBlueStar!
+        {
+            self.view.backgroundColor = UIColor.blueColor()
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.redColor()
+        }
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- UploadTest/ViewController.swift
+++ UploadTest/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let nextViewController = segue.destinationViewController as! ImageViewController
+        if segue.identifier == "blueStarSegue"
+        {
+          nextViewController.isBlueStar = true
+        }
+        else
+        {
+            nextViewController.isBlueStar = false
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        nextViewController.title = sender?.currentTitle
     }
-
-
+    
+    
 }
-
